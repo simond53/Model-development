10b5e04808cd9c23714feffd91e706858d32b631
Simon Dagenais <simon.dagenais@ibm.com>
2021-06-28 17:12:10 -0400

    initial commit

--- sample-front-app-main/README.md
+++
@@ -1,2 +0,0 @@
-# sample-front-app
-Sample frontend app for Cloud Engineer
--- sample-front-app-main/config/WMLService.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    apiKey: process.env.WML_API_KEY,
-    instanceId: process.env.WML_INSTANCE_ID,
-    modelDeploymentUrl: process.env.WML_MODEL_DEPLOYMENT_URL
-};
